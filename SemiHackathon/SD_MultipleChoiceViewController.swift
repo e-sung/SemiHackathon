7e0091b8195cea55c118e1efa8c056f313c0324c
esung <dev.esung@gmail.com>
2017-11-02 14:02:50 +0900

    Add SD_MultipleChoiceViewController

---
+++ SemiHackathon/SD_MultipleChoiceViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MultipleChoiceViewController.swift
+//  SemiHackathon
+//
+//  Created by esung on 2017. 11. 2..
+//  Copyright © 2017년 esung. All rights reserved.
+//
+
+import UIKit
+
+class SD_MultipleChoiceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
